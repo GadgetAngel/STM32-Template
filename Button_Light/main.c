3819522146862a69d5a6a82f02a3e3136d607009
GadgetAngel <joanmanges@gmail.com>
2021-02-04 21:25:33 -0500

    recompiled

--- Button_Light/main.c
+++ Button_Light/main.c
@@ -2,7 +2,8 @@
 
 //global variables
 //for testing purposes
-int button = 0;
+typedef enum {false, true} bool;
+bool button = false;
 
 void delay(void) {
   int i = 100000; /* About 1/4 second delay */
@@ -16,7 +17,7 @@ int main(void) {
   //Enable the clocks for GPIOC and GPIOA 0x10
   /* Enable the GPIOA [PA0 is the button] (bit 2) and GPIOC [PC8 (blue LED)] (bit 4) */
   /* See 7.3.7 in stm32f100x reference manual of RM0008*/
-  RCC ->APB2ENR |= 0x04 | 0x10;
+  RCC ->APB2ENR |= (0x04 | 0x10);
 
   //Configure the Button
   /* Set GPIOA Pin 0 to input floating */
@@ -35,18 +36,20 @@ int main(void) {
 
     /* see 9.2.3 in stm32f100x reference manual of RM0008*/
     // Read the button - the button pulls down PA0 to logic 0
-    //button = ((GPIOA ->IDR & 0x1) == 0);
-
-    if (((GPIOA ->IDR & 0x1) == 0)) {
-      // (button circuit is active low) (since active low, then reset the bit to turn ON the light)
+    //(button circuit is active low) so change the logic
+    //when the button is pressed the if statement will evaluate to true
+    if (((GPIOA ->IDR & 0x1) == 0) == 0) {
+      button = true;  //when the button is pressed this will be true
+      //  turn ON the light, PC8 is the blue LED
       /* see 9.2.5 in stm32f100x reference manual of RM0008*/
       //LED on PC8
-      GPIOC ->BSRR = 1 << 24;
+      GPIOC ->BSRR = 1 << 8;
     }
     else {
+      button = false;  //when the button is not pressed this will be false
       /* see 9.2.5 in stm32f100x reference manual of RM0008*/
-      //LED off PC8 (since circuit is active low, set the bit to turn off the light)
-      GPIOC ->BSRR = 1 << 8;
+      //turn off the light, PC8 is the blue LED
+      GPIOC ->BSRR = 1 << 24;
     }
   }
 }
